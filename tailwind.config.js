8962a74887dbdc55c9d56c02393ce03aec3c4d9d
blessyparida <paridablessy@gmail.com>
2025-11-15 12:31:09 +0530

    fixed css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/frontend/**/*.{js,ts,jsx,tsx,mdx}",
+    
   ],
   theme: {
     extend: {},
