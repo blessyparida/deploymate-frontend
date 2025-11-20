5831e759289252deb32b468ec0dc3e70b1366547
blessyparida <paridablessy@gmail.com>
2025-11-20 20:21:04 +0530

    fixed next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // any other options you had before
+
+  async redirects() {
+    return [
+      {
+        source: "/",          // when someone visits homepage
+        destination: "/github", // send them to /github
+        permanent: false,     // or true if you want SEO to treat it as fixed
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
